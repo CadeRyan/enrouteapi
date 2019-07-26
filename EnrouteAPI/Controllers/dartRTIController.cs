c2359050e352b90d427364b77d834ec1535db8b6
Cade Ryan <caryan@microsoft.com>
2019-07-26 15:52:04 +0100

    handling dart results

--- EnrouteAPI/Controllers/dartRTIController.cs
+++ EnrouteAPI/Controllers/dartRTIController.cs
@@ -3,65 +3,37 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using System.Xml;
+using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EnrouteAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class dartRTIController : ControllerBase
+    public class DartRTIController : ControllerBase
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(String id)
+        public string Get(string id)
         {
-            //WebRequest request = WebRequest.Create($"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc={id}");
+            WebRequest request = WebRequest.Create($"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc={id}");
 
-            //using (var sr = new StreamReader(request.GetResponse().GetResponseStream()))
-            //{
-            //    var brf = BusRouteQuery.FromJson(sr.ReadToEnd());
+            using (var sr = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(ArrayOfObjStationData));
+                ArrayOfObjStationData arrayOfObjStationData = (ArrayOfObjStationData)serializer.Deserialize(sr);
 
+                DartRTIOutput output = new DartRTIOutput();
 
-            //    return JsonConvert.SerializeObject(brf, Formatting.Indented);
+                foreach(var result in arrayOfObjStationData.ObjStationData)
+                {
+                    output.Results.Add(new DartRTIOutput.DartRTIResult(result.Traincode, result.Stationfullname, result.Stationcode,
+                        result.Origin, result.Destination, result.Origintime, result.Destinationtime, result.Status,
+                        result.Lastlocation, result.Duein, result.Late, result.Exparrival, result.Expdepart, result.Scharrival,
+                        result.Schdepart, result.Direction, result.Traintype, result.Locationtype));
+                }
 
-            //}
-
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load($"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc={id}");
-            string jsonText = JsonConvert.SerializeXmlNode(xmlDoc);
-
-           
-            return jsonText;
-
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                return JsonConvert.SerializeObject(output, Formatting.Indented); ;
+            }
         }
     }
 }
