1b5a9210ee0dd0b40bba0f40e9793b0624075a3c
Cade Ryan <caryan@microsoft.com>
2019-07-29 15:03:51 +0100

    re-writing allstop query

---
+++ EnrouteAPI/AllStopsOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnrouteAPI
+{
+    class AllStopsOutput
+    {
+        public AllStopsOutput()
+        {
+            BusStopDataResults = new List<StopReduced>();
+        }
+        public List<StopReduced> BusStopDataResults { get; set; }
+
+        public class StopReduced
+        {
+            public StopReduced(long stopid, string shortname, string fullname, string latitude, string longitude)
+            {
+                Stopid = stopid;
+                Shortname = shortname;
+                Fullname = fullname;
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+            public long Stopid { get; set; }
+            public string Shortname { get; set; }
+            public string Fullname { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+        }
+    }
+}
--- EnrouteAPI/BusRouteQuery.cs
+++ EnrouteAPI/BusRouteQuery.cs
@@ -9,8 +9,6 @@
 namespace EnrouteAPI
 {
     using System;
-    using System.Collections.Generic;
-
     using System.Globalization;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
